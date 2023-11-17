1e43aa2b60dc072019a2143efd6b69031c62286b
pritesh-52 <priteshbhatiya52@gmail.com>
2023-11-17 12:00:54 +0530

    basic level programe

---
+++ Part_1/26.js
@@ -0,0 +1,23 @@
+/* for of loop */
+let cl=["GLS","NIRMA","DDU"];
+
+for(let cl1 of cl)
+{
+    console.log(cl1);
+}
+
+/* for in loop */  /*  used to itetarciting give then index number */
+
+let gt=["ONE","TWO","THREE"]
+for (let cl5 in gt)
+{
+    console.log(cl5);
+}
+
+const fruits = ["apple", "mango", "grapes", "fruit4", "fruit5"];
+const fruits2 = [];
+
+for(let index in fruits){
+    fruits2.push(fruits[index].toUpperCase());
+}
+console.log(fruits2);
