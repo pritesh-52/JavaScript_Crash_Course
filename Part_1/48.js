6c20085257cc5bd8c60dc03f4a24571b4b5085e6
pritesh-52 <priteshbhatiya52@gmail.com>
2023-11-20 12:24:18 +0530

    basic leval program

--- Part_1/48.js
+++ Part_1/48.js
@@ -1,1 +1,10 @@
 /* Reduce method */
+
+
+const numbers=[1,2,3,4,5,6]
+
+
+const sumnum=numbers.reduce((intial,curre)=>{
+    return intial+curre;
+})
+console.log(sumnum);
---
+++ Part_1/49.js
@@ -0,0 +1,9 @@
+/* Sort Method */
+
+
+
+
+let sortdata=[10,1,5,8,2,4,0]
+
+
+console.log(sortdata.sort());
