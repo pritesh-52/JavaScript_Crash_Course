80a342e1ccbfd0271085b9851ee99b5b041e6612
pritesh-52 <priteshbhatiya52@gmail.com>
2023-12-20 09:05:36 +0530

    Part_2

---
+++ Part_2/arrowfunction.js
@@ -0,0 +1,34 @@
+const addtion=(a,b)=>a+b;
+const answer=addtion(20,30);
+
+console.log(answer);
+
+
+
+const person=()=>{
+    console.log("Hello");
+}
+person();
+
+
+
+arr=[10,20,30,40,50];
+
+const squremethod=arr.map((num)=>num+1);   //khown as arrow function
+console.log(squremethod);
+
+
+eventnumber=[10,12,13,14,18,16,19,21]
+const filterven=eventnumber.filter((num)=>num%2==0);
+console.log(filterven);
+
+
+// find method in js 
+num1=[10,20,30,40]
+const findnumber=num1.find(num1=>num1>10);
+console.log(findnumber)
+
+
+
+
+
--- Part_2/functionreturn.js
+++ Part_2/functionreturn.js
@@ -3,3 +3,14 @@ function fun()
     console.log("Pritesh Bhatiya")
 }
 fun()
+
+
+
+function myFunction(power){
+    return function(number){
+        return number ** power
+    }
+}
+const square = myFunction(2);
+const ans = square(3);
+console.log(ans);
